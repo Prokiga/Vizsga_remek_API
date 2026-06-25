9478133b982af705cae049b960a66b4f26fb95ab
Prokai Gábor <prokiga@gmail.com>
2026-06-25 22:05:53 +0200

    Add models

---
+++ WebAPI/Model/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+
+        }
+
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("SERVER=localhost;DATABASE=szines_negy_evszak;Uid=root;PWD=");
+        }
+    }
+}
---
+++ WebAPI/Model/users.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Model
+{
+    public class users
+    {
+        [Key]
+        public int user_id { get; set; }
+        [MaxLength(50)]
+        public string user_name { get; set; }
+        [MaxLength(50)]
+        public string pass_word { get; set; }
+    }
+}
