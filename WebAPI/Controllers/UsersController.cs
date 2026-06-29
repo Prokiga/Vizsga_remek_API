00054d4583ae4758859b6d8293c8d64ef0659684
Prokai Gábor <prokiga@gmail.com>
2026-06-29 21:49:17 +0200

    POST végpont készen van.

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        DatabaseContext db_context = new DatabaseContext();
+
+        // Post végpont létrehozása a felhasználó regisztrációjához
+        [HttpPost]
+        public ActionResult RegisterNewUser(AddNewUserDTO newUserDTO)
+        {
+            // Itt lehetne a felhasználó regisztrációs logikáját implementálni
+            try
+            {
+                var newUser = new users
+                {
+                    user_name = newUserDTO.user_name,
+                    pass_word = newUserDTO.pass_word
+                };
+
+                db_context.users.Add(newUser);
+                db_context.SaveChanges();
+
+                return StatusCode(StatusCodes.Status201Created, new { message = "User registered successfully.", user = newUser });
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occurred while registering the user.", ex.Message });
+                throw;
+            }
+        }
+    }
+}
---
+++ WebAPI/Model/AddNewUserDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Model
+{
+    public class AddNewUserDTO
+    {
+        public string user_name { get; set; }
+        [MaxLength(10)]
+        public string pass_word { get; set; }
+    }
+}
